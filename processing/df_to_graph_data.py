3a97f2b8da30fbb8f80175e1b6d6109f9cdffabf
PranavM112 <33048763+PranavM112@users.noreply.github.com>
2019-09-14 20:20:59 -0400

    speed vs time to csv implemented again and better this time again

--- processing/df_to_graph_data.py
+++ processing/df_to_graph_data.py
@@ -3,22 +3,16 @@ import numpy as np
 import pandas as pd
 import raw_data_processing as rdp
 
-def write_speed_vs_time_data_to_csv(data):
-    
+def write_time_vs_speed_data_to_csv(data):
     table = rdp.get_table(data)
-    cols_wanted = ['timestamp','vehicle_speed',     'fuel_consumed_since_restart','odometer']
+    cols_wanted = ['timestamp','vehicle_speed', 'fuel_consumed_since_restart','odometer']
     cols_to_delete = []
     for col in list(table):
         if col not in cols_wanted:
             cols_to_delete.append(col)
     table = table.drop(cols_to_delete, axis=1)
     table = rdp.remove_nan_from_table(table, cols_wanted)
-    mpg = [0]
-    for r in range(1,len(table)):
-        delta_d = table['odometer'][r] - table['odometer'][r]
-        delta_f = table['fuel_consumed_since_restart'][r] - table['odometer'][r]
-        inst_mpg = delta_d/delta_f
-        mpg.append(inst_mpg)
+    mpg = [0] + list(np.diff(table['odometer']))
     table['instantaneous_mpg'] = mpg
     table.to_csv('time_v_speed.csv')
     return table
