e2cb4bab02abca393a93dcefd8a6a785f131df53
Georgia Shay <georgiashay@gmail.com>
2019-09-15 01:21:29 -0400

    Add subcity shell and routing

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,16 @@ var express = require('express');
 var router = express.Router();
 const readCSV = require('./../public/javascripts/readCSV.js')
 
+
+var capitalize = function(word) {
+  return word[0].toUpperCase() + word.substring(1, word.length).toLowerCase();
+}
+
+var formatName = function(underscoredName) {
+  parts = underscoredName.split('_');
+  return parts.map((p)=>capitalize(p)).join(' ');
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var names = ['Aggressive Driving', 'Commute', 'Highway Speeding', 'Idling', 'Riding Brakes', 'Tailgating'];
@@ -20,6 +30,12 @@ router.get('/', function(req, res, next) {
 
 router.get('/cities', function(req, res, next) {
   res.render('cities', {title: 'Cities'});
+})
+
+router.get('/cities/:cityName/:subCityName', function(req, res, next) {
+  var cityName = formatName(req.params.cityName);
+  var subCityName = formatName(req.params.subCityName);
+  res.render('subcity', { title: cityName + ' ' + subCityName, city: cityName, subcity: subCityName, speedMPGData: {} });
 })
 
 module.exports = router;
