5e41ce13aeb81f60f4085a4b9f7b9b3318657543
Kevin Du <kevin_du@brown.edu>
2019-09-15 01:08:40 -0400

    cities kevin

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,4 +9,11 @@ router.get('/', function(req, res, next) {
   });
 });
 
+/* GET cities page. */
+router.get('/cities', function(req, res, next) {
+  readCSV.readCSV('cities_avg_mpg.csv', 200).then(function(data) {
+    res.render('cities', { title: 'Express', mpg_per_city: data});
+  });
+});
+
 module.exports = router;
