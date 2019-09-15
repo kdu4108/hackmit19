c9e93448631d11f69773cb6fbf808256807513f1
Georgia Shay <georgiashay@gmail.com>
2019-09-14 23:46:31 -0400

    Function to draw bar chart

--- server/public/javascripts/line_demo_1.js
+++ server/public/javascripts/line_demo_1.js
@@ -3,6 +3,25 @@ var speed = timeVsSpeed.vehicle_speed;
 var firstTime = time[0];
 time = time.map((t)=>moment.unix(t-firstTime).format('m:ss'));
 
+var nthColor = function(n, total) {
+  var colors = Object.keys(window.chartColors);
+  n = n % colors.length;
+  if (total != undefined) {
+    spacing = colors.length/total;
+    n = parseInt(n * spacing);
+  }
+  return window.chartColors[colors[n]];
+}
+
+var randomArray = function(n) {
+  arr = [];
+  for (var i = 0; i < n; i++) {
+    arr.push(randomScalingFactor());
+  }
+  return arr;
+}
+
+
 var drawLineChart = function(x, ys, title, canvasID) {
   var config = {
       type: 'line',
@@ -11,8 +30,8 @@ var drawLineChart = function(x, ys, title, canvasID) {
           datasets: ys.map(function(y, i) {
             return {
               label: x.title + ' vs' + y.title,
-              backgroundColor: window.chartColors[Object.keys(window.chartColors)[i]],
-              borderColor: window.chartColors[Object.keys(window.chartColors)[i]],
+              backgroundColor: nthColor(i, ys.length),
+              borderColor: nthColor(i, ys.length),
               data: y.data,
               fill: false
             }
@@ -59,49 +78,42 @@ var drawLineChart = function(x, ys, title, canvasID) {
   new Chart(ctx, config);
 }
 
-var drawBarChart=function(canvasID) {
-    var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var drawBarChart = function(x, ys, title, canvasID) {
     var color = Chart.helpers.color;
-    var barChartData = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Dataset 1',
-            backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
-            borderColor: window.chartColors.red,
-            borderWidth: 1,
-            data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor()
-            ]
-        }, {
-            label: 'Dataset 2',
-            backgroundColor: color(window.chartColors.blue).alpha(0.5).rgbString(),
-            borderColor: window.chartColors.blue,
-            borderWidth: 1,
-            data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor()
-            ]
-        }]
-    };
+    var config = {
+      type: 'bar',
+      data: {
+          labels: x.labels,
+          datasets: ys.map(function(y, i) {
+            return {
+              label: y.title,
+              backgroundColor: color(nthColor(i, ys.length)).alpha(0.5).rgbString(),
+              borderColor: nthColor(i, ys.length),
+              borderWidth: 1,
+              data: y.data
+            }
+          }),
+      },
+      options: {
+        responsive: true,
+        legend: {
+          display: ys.length > 1,
+          position: 'top'
+        },
+        title: {
+          display: true,
+          text: title
+        }
+      }
+    }
     var ctx = document.getElementById(canvasID).getContext('2d');
-    new Chart(ctx, barChartData);
+    new Chart(ctx, config);
 }
 
 
 window.onload = function() {
   drawLineChart({title: 'Time (minutes)', data: time}, [{title: 'Speed (kph)', data: speed}], 'Highway Speed', 'canvas');
-  this.drawBarChart('newCanvas')
+  drawBarChart({title: 'Example', labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July']}, [{title: 'Dataset 1', data: randomArray(7)}, {title: 'Dataset 2', data: randomArray(7)}], 'Example Bar Chart', 'newCanvas')
   // drawLineChart(time, speed, 'Highway Speed', 'Time (minutes)', 'Speed (kph)', false, 'canvas');
     // var ctx = document.getElementById('canvas').getContext('2d');
     // window.myLine = new Chart(ctx, config);
